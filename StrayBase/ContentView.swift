11b776032827b7010a1ca12dee9e6e7ef7bbd607
Nino Nozadze <nozadze.nino@bog.ge>
2025-03-21 14:31:08 +0400

    Initial Commit

---
+++ StrayBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 21.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StrayBase/StrayBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrayBaseApp.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 21.03.25.
+//
+
+import SwiftUI
+
+@main
+struct StrayBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
