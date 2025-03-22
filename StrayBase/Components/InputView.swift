f33b695a90794eeeba6b23b29123c1cd12cf9e32
Nino Nozadze <nozadze.nino@bog.ge>
2025-03-22 22:39:38 +0400

    configure login page

---
+++ StrayBase/Components/InputView.swift
@@ -0,0 +1,43 @@
+//
+//  InputView.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 22.03.25.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecuredField: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecuredField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            } else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            
+            Divider()
+        }
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView(text: .constant(""),
+                  title: "Email Address",
+                  placeholder: "name@example.com")
+    }
+}
---
+++ StrayBase/Core/Authentication/LoginView.swift
@@ -0,0 +1,78 @@
+//
+//  LoginView.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 22.03.25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        
+        NavigationStack {
+            VStack {
+                Image("სახელი") // TODO: add image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
+                
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecuredField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                Button {
+                    print("asdf")
+                } label: {
+                    HStack {
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32,
+                           height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+                
+            }
+        }
+        
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
