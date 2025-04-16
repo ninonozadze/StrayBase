c90dbf7cb8fa094ee0d27bc7fd74d9182ae33beb
Nino Nozadze <nozadze.nino@bog.ge>
2025-04-16 15:20:03 +0400

    implement fetch user

--- StrayBase/Core/Authentication/ViewModel/AuthViewModel.swift
+++ StrayBase/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -16,6 +16,10 @@ class AuthViewModel: ObservableObject {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        
+        Task {
+            await fetchUser()
+        }
     }
     
     func signIn(withEmail email: String,
@@ -39,6 +43,7 @@ class AuthViewModel: ObservableObject {
             )
             let encodedUser = try Firestore.Encoder().encode(user)
             try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            await fetchUser()
             
         } catch {
             print("DEBUG: Failed to create user with error - \(error.localizedDescription)")
@@ -54,6 +59,10 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
+        self.currentUser = try? snapshot.data(as: User.self)
         
+        print("DEBUG: Current user is \(self.currentUser)")
     }
 }
--- StrayBase/Core/Profile/ProfileView.swift
+++ StrayBase/Core/Profile/ProfileView.swift
@@ -10,69 +10,75 @@ import SwiftUI
 private typealias ProfileViewConsts = SharedUtils.AuthenticationViews.ProfileView
 
 struct ProfileView: View {
+    
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List {
-            Section {
-                HStack {
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(.circle)
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullname)
-                            .font(.headline)
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(.circle)
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullname)
+                                .font(.headline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                Section(ProfileViewConsts.generalSectionTitle) {
+                    HStack {
+                        SettingsRowView(
+                            imageName: ProfileViewConsts.versionImageName,
+                            title: ProfileViewConsts.versionTitle,
+                            tintColor: Color(.systemGray)
+                        )
+                        
+                        Spacer()
+                        
+                        Text("1.0.0") // TODO: tmp
+                            .font(.subheadline)
                             .foregroundColor(.gray)
                     }
                 }
-            }
-            
-            Section(ProfileViewConsts.generalSectionTitle) {
-                HStack {
-                    SettingsRowView(
-                        imageName: ProfileViewConsts.versionImageName,
-                        title: ProfileViewConsts.versionTitle,
-                        tintColor: Color(.systemGray)
-                    )
-                    
-                    Spacer()
-                    
-                    Text("1.0.0") // TODO: tmp
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-            }
-            
-            Section(ProfileViewConsts.accountSectionTitle) {
-                Button {
-                    print("sign out") // TODO: tmp
-                } label: {
-                    SettingsRowView(
-                        imageName: ProfileViewConsts.signOutImageName,
-                        title: ProfileViewConsts.signOutTitle,
-                        tintColor: .red
-                    )
-                }
                 
-                Button {
-                    print("delete") // TODO: tmp
-                } label: {
-                    SettingsRowView(
-                        imageName: ProfileViewConsts.deleteAccountImageName,
-                        title: ProfileViewConsts.deleteAccountTitle,
-                        tintColor: .red
-                    )
+                Section(ProfileViewConsts.accountSectionTitle) {
+                    Button {
+                        print("sign out") // TODO: tmp
+                    } label: {
+                        SettingsRowView(
+                            imageName: ProfileViewConsts.signOutImageName,
+                            title: ProfileViewConsts.signOutTitle,
+                            tintColor: .red
+                        )
+                    }
+                    
+                    Button {
+                        print("delete") // TODO: tmp
+                    } label: {
+                        SettingsRowView(
+                            imageName: ProfileViewConsts.deleteAccountImageName,
+                            title: ProfileViewConsts.deleteAccountTitle,
+                            tintColor: .red
+                        )
+                    }
                 }
             }
         }
+        
     }
 }
 
