f13e35d6f8b7a526a287ddb39ccb0b9104e61f53
Nino Nozadze <nozadze.nino@bog.ge>
2025-05-20 21:35:00 +0400

    ConfirmActionButton component

---
+++ StrayBase/Components/ConfirmActionButton.swift
@@ -0,0 +1,47 @@
+//
+//  ConfirmActionButton.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 20.05.25.
+//
+
+import SwiftUI
+
+struct ConfirmActionButton: View {
+    let viewModel: ConfirmActionButtonViewModel
+    @State private var showAlert = false
+
+    var body: some View {
+        Button {
+            showAlert = true
+        } label: {
+            SettingsRowView(
+                imageName: viewModel.imageName,
+                title: viewModel.title,
+                tintColor: viewModel.tintColor
+            )
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text(viewModel.alertTitle),
+                message: Text(viewModel.alertMessage),
+                primaryButton: .destructive(Text(viewModel.alertConfirmButtonTitle)) {
+                    Task {
+                        await viewModel.action()
+                    }
+                },
+                secondaryButton: .cancel()
+            )
+        }
+    }
+}
+
+struct ConfirmActionButtonViewModel {
+    let imageName: String
+    let title: String
+    let tintColor: Color
+    let alertTitle: String
+    let alertMessage: String
+    let alertConfirmButtonTitle: String
+    let action: () async -> Void
+}
--- StrayBase/Core/Profile/ProfileView.swift
+++ StrayBase/Core/Profile/ProfileView.swift
@@ -58,57 +58,37 @@ struct ProfileView: View {
                 }
                 
                 Section(ProfileViewConsts.accountSectionTitle) {
-                    Button {
-                        showSignOutAlert = true
-                    } label: {
-                        SettingsRowView(
-                            imageName: ProfileViewConsts.signOutImageName,
-                            title: ProfileViewConsts.signOutTitle,
-                            tintColor: .red
-                        )
-                    }
-                    .alert(isPresented: $showSignOutAlert) {
-                        Alert(
-                            title: Text(ProfileViewConsts.signOutAlertTitle),
-                            message: Text(ProfileViewConsts.signOutAlertDescription),
-                            primaryButton: .destructive(Text(ProfileViewConsts.signOutAlertButton)) {
-                                Task {
-                                    do {
-                                        try await viewModel.signOut()
-                                    } catch {
-                                        print("DEBUG: Failed to sign out with error - \(error.localizedDescription)")
-                                    }
-                                }
-                            },
-                            secondaryButton: .cancel()
-                        )
-                    }
-                    
-                    Button {
-                        showDeleteAlert = true
-                    } label: {
-                        SettingsRowView(
-                            imageName: ProfileViewConsts.deleteAccountImageName,
-                            title: ProfileViewConsts.deleteAccountTitle,
-                            tintColor: .red
-                        )
-                    }
-                    .alert(isPresented: $showDeleteAlert) {
-                        Alert(
-                            title: Text(ProfileViewConsts.deleteAlertTitle),
-                            message: Text(ProfileViewConsts.deleteAlertDescription),
-                            primaryButton: .destructive(Text(ProfileViewConsts.deleteAlertButton)) {
-                                Task {
-                                    do {
-                                        try await viewModel.deleteAccount()
-                                    } catch {
-                                        print("DEBUG: Failed to delete account with error - \(error.localizedDescription)")
-                                    }
-                                }
-                            },
-                            secondaryButton: .cancel()
-                        )
-                    }
+                    ConfirmActionButton(viewModel: .init(
+                        imageName: ProfileViewConsts.signOutImageName,
+                        title: ProfileViewConsts.signOutTitle,
+                        tintColor: .red,
+                        alertTitle: ProfileViewConsts.signOutAlertTitle,
+                        alertMessage: ProfileViewConsts.signOutAlertDescription,
+                        alertConfirmButtonTitle: ProfileViewConsts.signOutAlertButton,
+                        action: {
+                            do {
+                                try await viewModel.signOut()
+                            } catch {
+                                print("DEBUG: Failed to sign out with error - \(error.localizedDescription)")
+                            }
+                        }
+                    ))
+
+                    ConfirmActionButton(viewModel: .init(
+                        imageName: ProfileViewConsts.deleteAccountImageName,
+                        title: ProfileViewConsts.deleteAccountTitle,
+                        tintColor: .red,
+                        alertTitle: ProfileViewConsts.deleteAlertTitle,
+                        alertMessage: ProfileViewConsts.deleteAlertDescription,
+                        alertConfirmButtonTitle: ProfileViewConsts.deleteAlertButton,
+                        action: {
+                            do {
+                                try await viewModel.deleteAccount()
+                            } catch {
+                                print("DEBUG: Failed to delete account with error - \(error.localizedDescription)")
+                            }
+                        }
+                    ))
                 }
             }
         }
