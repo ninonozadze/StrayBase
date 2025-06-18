b492eb80de4c555d2f90d48d14e3cb4f301458c4
Nino Nozadze <nozadze.nino@bog.ge>
2025-06-18 18:24:10 +0400

    shared

---
+++ StrayBase/SharedUtils/SharedUtils + Components.swift
@@ -0,0 +1,15 @@
+//
+//  SharedUtils + Components.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 18.06.25.
+//
+
+extension SharedUtils {
+    
+    struct InputView {
+        static let hideIcon: String = "eye.slash"
+        static let unhideIcon: String = "eye"
+    }
+    
+}
---
+++ StrayBase/SharedUtils/SharedUtils + Tabs.swift
@@ -0,0 +1,73 @@
+//
+//  SharedUtils + Tabs.swift
+//  StrayBase
+//
+//  Created by Nino Nozadze on 18.06.25.
+//
+
+extension SharedUtils {
+    
+    enum tab: Hashable {
+        case dashboard, shelter, addStray, foster, profile
+    }
+    
+}
+
+extension SharedUtils.tab {
+    
+    var tabTitle: String {
+        switch self {
+        case .dashboard:
+            return "Home"
+        case .shelter:
+            return "Shelters"
+        case .addStray:
+            return "Add Stray"
+        case .foster:
+            return "Fosters"
+        case .profile:
+            return "Profile"
+        }
+    }
+
+    var tabImageName: String {
+        switch self {
+        case .dashboard:
+            return "house"
+        case .shelter:
+            return "mappin.and.ellipse.circle"
+        case .addStray:
+            return "pawprint"
+        case .foster:
+            return "heart.text.square"
+        case .profile:
+            return "person"
+        }
+    }
+    
+}
+
+extension SharedUtils {
+    
+    struct TabViews {
+        
+        struct ProfileView {
+            static let accountSectionTitle = "Account"
+            
+            static let signOutImageName = "arrow.left.circle.fill"
+            static let signOutTitle = "Sign Out"
+            
+            static let deleteAccountImageName = "xmark.circle.fill"
+            static let deleteAccountTitle = "Delete Account"
+            
+            static let deleteAlertTitle = "Delete Account"
+            static let deleteAlertDescription = "Are you sure you want to delete your account?"
+            static let deleteAlertButton = "Delete"
+            
+            static let signOutAlertTitle = "Sign out"
+            static let signOutAlertDescription = "Are you sure you want to sign out?"
+            static let signOutAlertButton = "Sign out"
+        }
+        
+    }
+}
